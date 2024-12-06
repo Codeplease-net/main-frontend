778bd6c9c77f59faf91ab3101ac343e0f154d7e3
Zhang Shan <46948684+Danchuong@users.noreply.github.com>
2024-12-06 16:46:49 +0700

    testing-deploy

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,6 @@ export default createMiddleware(routing);
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(en|de|zh-CN|zh-TW|ru|ja|ko|vi)/:path*']
+  matcher: ['/', '/(en|de|zh-CN|ko|vi)/:path*']
 };
 
